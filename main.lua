c0622031de665a764065614e90375d88147c0ec7
David Antúnez Glez <me@davidantunez.es>
2013-06-09 16:57:41 +0200

    ldoc comments updated

--- main.lua
+++ main.lua
@@ -1,3 +1,29 @@
+---------------
+-- ShootEmAll.  
+-- Prototype for löve 2d 0.8.0
+--
+-- [Github Project](https://github.com/eipporko/ShootEmAll)
+--
+--	Instructions
+--
+--	The goal of the game is to survive as long as you can.  
+--	When you kill a monster you can obtain cash and buy power ups or buildings.
+--
+--	Controls
+--	W: Forward
+--	S: Back
+--	A: Left
+--	D: Right
+--	Left Mouse Button: Fire Weapon
+--	P: Debug mode
+--	1-2: Set zoom
+--	Esc: Exit
+--
+-- @author David Antúnez González
+-- @copyright 2013
+-- @license GPLv3
+-- @script ShootEmAll
+
 require("table-save")
 require("level")
 
